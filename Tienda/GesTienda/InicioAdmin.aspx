23387bcc71833bdb74ad7b762c3c906f1bf04cb2
CristinaPicazo <cristina.picazo.merlos@gmail.com>
2025-01-03 13:55:20 +0100

    ejercicio 8

---
+++ Tienda/GesTienda/InicioAdmin.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="InicioAdmin" Language="C#" MasterPageFile="~/MasterPageAdm.Master" AutoEventWireup="true" CodeBehind="InicioAdmin.aspx.cs" Inherits="GesTienda.InicioAdmin" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="InfoContenido" runat="server">
+        <div id="cabecera1" class="contenidotitulo">Inicio de Administrador</div>
+        <p>Esta es la página de inicio de los administradores. Bienvenido al Panel de Administración de la Aplicación Web GesTienda.</p>
+
+</asp:Content>
--- Tienda/GesTienda/MasterPage.Master.cs
+++ Tienda/GesTienda/MasterPage.Master.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -10,13 +11,19 @@ namespace Prueba
     public partial class MasterPage : System.Web.UI.MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
-        {            
-                
+        {
+            if(Convert.ToString(Session["Rol"]) != "U")
+            {
+                Response.Redirect("~/Default.aspx");
+            }
+            lblDatosUsuario.Text = Convert.ToString(Session["Nombre"]);
         }
 
         protected void btnCerrar_Click(object sender, EventArgs e)
         {
-            
+            Session.Clear();
+            Session.Abandon();
+            Response.Redirect("~/Default.aspx?Message=Sesion_cerrada\"", false);
         }
 
     }
